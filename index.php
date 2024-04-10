61ee0accb5ef99cc0a8180d1e7b186ecc9d9480c
Sebastian Köhler <github@pordi.de>
2024-04-10 21:48:00 +0200

    Add files via upload
    initial commit - works most of the time

---
+++ index.php
@@ -0,0 +1,410 @@
+<?php
+require_once('svg.php');
+$announce = array();
+$mainRoad = array();
+$puzzleId = array();
+define ('GRAS' , 'lightgreen');
+define ('ROAD' , 'grey');
+
+function generateSVG() {
+    $layout = generateRandomLayout();
+    $mainRoad = generateMainRoad($layout);
+    $svg = '<svg width="150" height="150" xmlns="http://www.w3.org/2000/svg">';
+    $x = 0;
+    $y = 0;
+    foreach ($layout as $index => $color) {
+        if($index < 4 or $index == 5 or $index == 6){
+            $svg .= '<rect x="' . $x . '" y="' . $y . '" width="50" height="50" fill="' . $color . '" />';
+        } elseif ($index == 7) {
+            $svg .= '<rect x="' . $x . '" y="' . $y . '" width="50" height="50" fill="' . $color . '"></rect>';
+            $svg .= generateBicycleSVG($x, $y);
+        } elseif ($index == 8) {
+            $svg .= '<rect x="' . $x . '" y="' . $y . '" width="50" height="50" fill="' . $color . '"></rect>';
+            $svg .= insertTrafficSign($x, $y, $mainRoad);
+        } elseif ($index == 4) {
+            $svg .= '<rect x="' . $x . '" y="' . $y . '" width="50" height="50" fill="' . $color . '"></rect>';
+            // Zufällige Richtung für den Pfeil wählen
+            $svg .= generateArrowSVG($x, $y, $layout);
+        }
+        $x += 50;
+        if (($index + 1) % 3 == 0) {
+            $x = 0;
+            $y += 50;
+        }
+
+    }
+    $x = 0;
+    $y = 0;
+
+    foreach($layout as $index => $color) {
+        if($index==3 && $layout[3] == 'grey'){
+            $svg .= generateCar($x, $y, 'lightblue', $layout);
+        }
+        if($index==5 && $layout[5] == 'grey'){
+            $svg .= generateCar($x, $y, 'lightgreen', $layout);
+        }
+        if($index==1 && $layout[1] == 'grey'){
+            $svg .= generateCar($x, $y, 'pink', $layout);
+        }
+
+        $x += 50;
+        if (($index + 1) % 3 == 0) {
+            $x = 0;
+            $y += 50;
+        }
+
+    }
+    // Weitere Logik zur Hinzufügung von Fahrzeugen, Verkehrszeichen usw. hier einfügen
+    $svg .= '</svg>';
+    return $svg;
+}
+
+function announce($from, $to){
+    global $announce;
+    $announce[$from] = $to;
+}
+
+function insertTrafficSign($x, $y, $mainRoad){
+    $sign = '';
+    $x_rand = rand(0,1);
+    if(isset($_GET['sign'])){
+        $x_rand = $_GET['sign'];
+    }
+    storeData('sign', $x_rand);
+    if($mainRoad) {
+        if(in_array(7, $mainRoad)){
+            $sign = generateMainRoadSignSVG($x, $y, $mainRoad);
+        } else {
+            if ($x_rand == 0) $sign = generateGiveWaySignSVG($x, $y, $mainRoad);
+            if ($x_rand == 1) $sign = generateStopSignSVG($x, $y, $mainRoad);
+        }
+    }
+    return $sign;
+}
+
+function storeData($what, $value){
+    global $puzzleId;
+    $puzzleId[$what]=$value;
+}
+
+function guessRightOfWay($mainRoad, $announce){
+
+    // priorities =
+    // 00 - policeman
+    // 10 - no intersecting traffic
+    // 10 - green traffic lights
+    // 20 - stay on main road
+    // 30 - leave main road
+    // 40 - not turning left
+    // 50 - no one right of me
+    // 60 - stop sign
+    // 70 - red traffic lights
+
+    $done = array();
+    $carPrios = array();
+
+    foreach($announce as $from=>$to){
+        if($from=='stop') continue;
+        // car stays on main road - prio 0
+        $x = array($from, $to);
+        if(sizeof(array_intersect($x, $mainRoad)) == 2){
+            $carPrios[$from] = 20;
+            $done[$from]=$from;
+        }
+
+        // car comes from main road - but diverts.. - prio 1
+        if(!isset($done[$from]) && sizeof(array_intersect(array($from), $mainRoad)) == 1){
+            // intersect with other in same category??
+            $carPrios[$from] = 30;
+            $done[$from]=$from;
+        }
+    }
+
+    $intersect = array();
+    foreach($announce as $from=>$to){
+        if($from=='stop') continue;
+        switch($from){
+            case 1:
+                $intersect[0][$from]=$from;
+                if($to==5){
+                    $intersect[2][$from]=$from;
+                }
+                break;
+            case 3:
+                $intersect[2][$from]=$from;
+                if($to==1){
+                    $intersect[3][$from]=$from;
+                }
+                break;
+            case 5:
+                $intersect[1][$from]=$from;
+                if($to==7){
+                    $intersect[0][$from]=$from;
+                }
+                break;
+            case 7:
+                $intersect[3][$from]=$from;
+                if($to==3){
+                    $intersect[1][$from]=$from;
+                }
+                break;
+        }
+        switch($to){
+            case 1:
+                $intersect[1][$from]=$from;
+                break;
+            case 3:
+                $intersect[0][$from]=$from;
+                break;
+            case 5:
+                $intersect[3][$from]=$from;
+                break;
+            case 7:
+                $intersect[2][$from]=$from;
+                break;
+        }
+    }
+
+    // find cars with no intersecting traffic -
+    foreach($announce as $from=>$to){
+        if($from=='stop') continue;
+        $canGo = true;
+        foreach($intersect as $corner){
+            if(in_array($from, $corner) && sizeof($corner)>1){
+                $canGo = false;
+            }
+        }
+        if($canGo){
+            // not intersecting - but STOP sign 8applies only on car 7 = bike)
+            if(isset($announce['stop']) && $from == 7){
+                $carPrios[$from] = 61;
+            } else {
+                // no intersecting traffic - prio 0
+                $carPrios[$from] = 'anytime';
+            }
+            $done[$from]=$from;
+        }
+    }
+
+
+    if(isset($announce['stop'])){
+        $carPrios['stop'] = 0;
+    }
+
+    foreach($announce as $from=>$to){
+        if($from=='stop') continue;
+        $isLeftTurn = ($from == 1 && $to == 5) || ($from == 5 && $to == 7) ||
+            ($from == 7 && $to == 3) || ($from == 3 && $to == 1);
+        if(!$isLeftTurn && !isset($done[$from])){
+            $done[$from] = $from;
+            $carPrios[$from] = 40;
+        }
+    }
+
+    foreach($announce as $from=>$to){
+        if($from=='stop') continue;
+        $rightIsFree = ($from == 1 && !$announce[3]) || ($from == 5 && !$announce[1]) ||
+            ($from == 7 && !$announce[5]) || ($from == 3 && $announce[7]);
+        if($rightIsFree && !isset($done[$from])){
+            $done[$from] = $from;
+            $carPrios[$from] = 50;
+        }
+    }
+
+    // check collision on 30 prio
+    $carsOn30 = array();
+    foreach($carPrios as $car=>$p){
+        if($p==30) $carsOn30[] = $car;
+    }
+    if(sizeof($carsOn30)>1){
+        //echo "carsOn30 = ".implode(', ',$carsOn30);
+        // check intersection sector 0
+        $x = array_intersect($intersect[0], $carsOn30);
+        $x = array_diff($x, array(1));
+        foreach ($x as $c){
+            //echo "in sector 0 have to wait : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+        // check intersection sector 1
+        $x = array_intersect($intersect[1], $carsOn30);
+        $x = array_diff($x, array(5));
+        foreach ($x as $c){
+            //echo "in sector 1 have to wait : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+        // check intersection sector 2
+        $x = array_intersect($intersect[2], $carsOn30);
+        $x = array_diff($x, array(3));
+        foreach ($x as $c){
+            //echo "in sector 2 : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+        // check intersection sector 3
+        $x = array_intersect($intersect[3], $carsOn30);
+        $x = array_diff($x, array(7));
+        foreach ($x as $c){
+            //echo "in sector 3 : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+    }
+
+    // check collision on 30 prio
+    $carsOn40 = array();
+    foreach($carPrios as $car=>$p){
+        if($p==40) $carsOn40[] = $car;
+    }
+    if(sizeof($carsOn40)>1){
+        //echo "carsOn40 = ".implode(', ',$carsOn40);
+        // check intersection sector 0
+        $x = array_intersect($intersect[0], $carsOn40);
+        $x = array_diff($x, array(1));
+        foreach ($x as $c){
+            //echo "in sector 0 have to wait : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+        // check intersection sector 1
+        $x = array_intersect($intersect[1], $carsOn40);
+        $x = array_diff($x, array(5));
+        foreach ($x as $c){
+            //echo "in sector 1 have to wait : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+        // check intersection sector 2
+        $x = array_intersect($intersect[2], $carsOn40);
+        $x = array_diff($x, array(3));
+        foreach ($x as $c){
+            //echo "in sector 2 : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+        // check intersection sector 3
+        $x = array_intersect($intersect[3], $carsOn40);
+        $x = array_diff($x, array(7));
+        foreach ($x as $c){
+            //echo "in sector 3 : ".implode($x)."<br>\n";
+            $carPrios[$c]++;
+        }
+    }
+
+    if(!$carPrios[1] && $announce[1]) $carPrios[1] = 99;
+    if(!$carPrios[3] && $announce[3]) $carPrios[3] = 99;
+    if(!$carPrios[5] && $announce[5]) $carPrios[5] = 99;
+    if(!$carPrios[7] && $announce[7]) $carPrios[7] = 99;
+
+    asort($carPrios);
+
+
+    // input is a dynamic array of 1 to 4 cars with same priority
+
+    return $carPrios;
+}
+
+
+
+echo "Punkte <span id='p'></span> - Highscore <span id='h'></span><br>";
+echo "<A href='".$url."' target='_blank'>Puzzle Url</A><br><hr>";
+echo generateSVG();
+$prio = guessRightOfWay($mainRoad, $announce);
+echo '<hr><pre>';
+print_r(
+    array(
+        //'announce'=>$announce,
+        //'mainRoad'=>$mainRoad,
+        'prio'=>$prio
+    )
+);
+$url = '?';
+foreach($puzzleId as $key=> $val){
+    $url.=$key.'='.$val.'&';
+}
+echo "<script> var data = JSON.parse('".json_encode($prio)."');
+var p=0;
+var h=0;
+h = getCookie('h');
+p = getCookie('p');
+
+document.getElementById('h').innerHTML=h;
+document.getElementById('p').innerHTML=p;
+function clickOn(id){
+    if(checkCollision(id)) {
+            document.getElementById('car'+id).style.display = 'none';
+            document.getElementById('carpath'+id).style.display = 'none';
+            delete data[id];
+            if(id == 7){
+                setCookie('p', ++p, 4);
+                if(p>h) setCookie('h', p, 4);
+                //alert('Alles richtig! Jetzt darfst Du fahren');
+                location.href='?';
+                exit;
+            }
+    } else {
+        alert('Es gab einen schrecklichen Unfall!');
+        p = 0;
+        setCookie('p', 0, 4);
+    }
+}
+
+function noSmallerValues(obj, key) {
+    for (let k in obj) {
+        if (obj.hasOwnProperty(k) && k !== key && obj[k] < obj[key] && obj[k] != 'anytime' && obj[k] != 'stop') {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkCollision(id){
+    if (data[id] == 'anytime') return true;
+    if (noSmallerValues(data, id)) return true;
+}
+
+
+if(document.getElementById('car1') != undefined)
+document.getElementById('car1').addEventListener('click', function() {
+    clickOn(1);
+});
+
+if(document.getElementById('car3') != undefined)
+document.getElementById('car3').addEventListener('click', function() {
+    clickOn(3);
+});
+
+if(document.getElementById('car5') != undefined)
+document.getElementById('car5').addEventListener('click', function() {
+    clickOn(5);
+});
+
+if(document.getElementById('car7') != undefined)
+document.getElementById('car7').addEventListener('click', function() {
+    clickOn(7);
+});
+
+if(document.getElementById('stop') != undefined)
+document.getElementById('stop').addEventListener('click', function() {
+    delete data['stop'];
+    this.style.fill='green';
+});
+
+";
+echo '
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(";");
+    for(var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === " ") cookie = cookie.substring(1, cookie.length);
+        if (cookie.indexOf(nameEQ) === 0) return cookie.substring(nameEQ.length, cookie.length);
+    }
+    return null;
+}
+';
+echo "</script>";
---
+++ svg.php
@@ -0,0 +1,295 @@
+<?php
+function generateGiveWaySignSVG($x, $y, $mainRoad) {
+    $svg = '';
+    $svg .= '<polygon points="' . ($x + 12.5) . ',' . ($y + 20) . ' ' . ($x+5) . ',' . ($y+5) . ' ' . ($x + 20) . ',' . ($y+5) . '" fill="white" stroke="red" stroke-width="2" />';
+    $svg .= '';
+    $svg .= generateMainRoadAddOn($x, $y, $mainRoad);
+    return $svg;
+}
+
+function generateMainRoadSignSVG($x, $y, $mainRoad) {
+    $svg = '';
+    $svg .= '<rect x="' . ($x + 5) . '" y="' . ($y + 5) . '" width="15" height="15" fill="white" stroke="yellow" stroke-width="2" transform="rotate(45 ' . ($x + 12.5) . ' ' . ($y + 12.5) . ')" />';
+    $svg .= '';
+    $svg .= generateMainRoadAddOn($x, $y, $mainRoad);
+    return $svg;
+}
+function generateRandomLayout() {
+    $layout = array_fill(0, 9, GRAS);
+    $layout[4] = ROAD;
+    $layout[7] = ROAD;
+    $random = array(1,1,1,0);
+    shuffle($random);
+    $layout[1] = $random[0]?ROAD:GRAS;
+    $layout[3] = $random[1]?ROAD:GRAS;
+    $layout[5] = $random[2]?ROAD:GRAS;
+    if(isset($_GET['layout'])){
+        $layout = array();
+        $items = preg_split('//', $_GET['layout'], -1, PREG_SPLIT_NO_EMPTY);
+        foreach($items as $item){
+            $layout[]=($item?ROAD:GRAS);
+        }
+    }
+    $layoutString = '';
+    foreach($layout as $l){
+        $layoutString.=($l==ROAD)?1:0;
+    }
+    storeData('layout', $layoutString);
+    return $layout;
+}
+function generateMainRoadAddOn($x, $y, $mainRoad) {
+    $svg = '';
+    // 1 3 5 7
+    $mainRoadString = implode('', $mainRoad);
+    if($mainRoadString == '35' || $mainRoadString == '17') {
+        if($mainRoadString == '35') {
+            // what da fuq?
+            //$svg .= '<path d="M'.($x+2+5).','.(27.5+$y+0+5).' '.(15+$x+5-2).','.(27.5+$y+0+5).'" fill="none" stroke="black" stroke-width="2" />';
+        }
+        if($mainRoadString == '17') {
+            // what da fuq?
+        }
+    } else {
+        $svg .= '<rect x="' . ($x + 5) . '" y="' . ($y + 25) . '" width="15" height="15" fill="white" stroke="black" stroke-width="0" />';
+        if($mainRoadString == '13') {
+            $svg .= '<path d="M'.(7.5+$x+2+5).','.(20+$y+2+5).' A7.5,7.5 0 0,1 '.($x+5+2).','.(27.5+$y+2+5).'" fill="none" stroke="black" stroke-width="2" />';
+        }
+        if($mainRoadString == '15') {
+            $svg .= '<path d="M'.(15+$x-2+5).','.(27.5+$y+2+5).' A7.5,7.5 0 0,1 '.($x+7.5-2+5).','.(20+$y+2+5).'" fill="none" stroke="black" stroke-width="2" />';
+        }
+        if($mainRoadString == '57') {
+            $svg .= '<path d="M'.($x+7.5-2+5).','.(35+$y-2+5).' A7.5,7.5 0 0,1 '.(15+$x-2+5).','.(27.5+$y-2+5).'" fill="none" stroke="black" stroke-width="2" />';
+        }
+        if($mainRoadString == '37') {
+            $svg .= '<path d="M'.($x+2+5).','.(27.5+$y-2+5).' A7.5,7.5 0 0,1 '.($x+7.5+2+5).','.(35+$y-2+5).'" fill="none" stroke="black" stroke-width="2" />';
+        }
+    }
+    return $svg;
+}
+
+function generateStopSignSVG($x, $y, $mainRoad) {
+    $svg = '';
+    $svg .= '<polygon id="stop" points="' . ($x + 8) . ',' . $y . ' ' . ($x + 17) . ',' . $y . ' ' . ($x + 24) . ',' . ($y + 7) . ' ' . ($x + 24) . ',' . ($y + 17) . ' ' . ($x + 17) . ',' . ($y + 24) . ' ' . ($x + 8) . ',' . ($y + 24) . ' ' . $x . ',' . ($y + 17) . ' ' . $x . ',' . ($y + 7) . '" fill="red" />';
+    $svg .= '<text x="' . ($x + 12.5) . '" y="' . ($y + 14) . '" font-family="Arial" font-size="7" fill="white" text-anchor="middle">STOP</text>';
+    $svg .= generateMainRoadAddOn($x, $y, $mainRoad);
+    announce('stop','stop');
+
+    return $svg;
+}
+
+// Funktion zur Generierung des SVG für ein Fahrrad
+function generateBicycleSVG($x, $y) {
+    $svg = '';
+    $svg .= '<rect id="car7" x="' . ($x+35) . '" y="' . ($y+10) . '" width="3" height="20" fill="white" />';
+    $svg .= '<rect x="' . ($x + 31) . '" y="' . ($y + 12) . '" width="11" height="2" fill="black" />';
+    $svg .= '';
+    return $svg;
+}
+
+function generateCar($x, $y, $color, $layout){
+    if($x==100){
+        $offsetX = 5;
+    }
+    if($x == 0) {
+        $offsetX = 10;
+    }
+    if($x==50) {
+        $offsetX = 5;
+    }
+    if($x==0){
+        $offsetY = 27;
+    } else {
+        $offsetY = 2;
+    }
+    if($y==50){
+        $height = 18;
+        $width = 35;
+    } else {
+        $height = 35;
+        $width = 18;
+    }
+
+    $svg = '';
+
+    if($x==0 && $y==50){
+        $svg .= generateLeftCarPath($color, $layout);
+        $id = 3;
+    }
+    if($x==50 && $y==0){
+        $svg .= generateTopCarPath($color, $layout);
+        $id = 1;
+    }
+    if($x==100 && $y==50){
+        $svg .= generateRightCarPath($color, $layout);
+        $id = 5;
+    }
+
+    $svg .= '<rect id="car'.$id.'" width="'.$width.'" height="'.$height.'" x="'.($x+$offsetX).'" y="'.($y+$offsetY).'" rx="2"
+      stroke="silver" fill="'.$color.'" stroke-width="1" />';
+    $svg .= '';
+
+
+    return $svg;
+
+}
+
+function generateLeftCarPath($color, $layout){
+    $x = 50; $y = 50;
+    $svg = '';
+    $possible = array();
+    if($layout[1]==ROAD){
+        $possible[] = 1;
+    }
+    if($layout[5]==ROAD){
+        $possible[] = 5;
+    }
+    if($layout[7]==ROAD){
+        $possible[] = 7;
+    }
+    shuffle($possible);
+    if(isset($_GET['left'])) $possible[0]=$_GET['left'];
+    switch($possible[0]){
+        case 5:
+            $svg .= '<polygon id="carpath3" points="' . ($x + 1) . ',' . ($y + 39) . ' ' . ($x + 49) . ',' . ($y + 39) . ' ' . ($x + 49) . ',' . ($y + 36). ' ' . ($x + 1) . ',' . ($y + 36) . '" fill="'.$color.'" />';
+            announce(3, 5);
+            break;
+        case 1:
+            $svg .= '<path id="carpath3" d="M'.($x+37.5).','.($y+1).' A37,37 0 0,1 '.($x+1).','.($y+37.5).'" fill="none" stroke="'.$color.'" stroke-width="3" />';
+            announce(3, 1);
+            break;
+        case 7:
+            $svg .= '<path id="carpath3" d="M'.($x+1).','.($y+37.5).' A12.5,12.5 0 0,1 '.($x+12.5).','.($y+49).'" fill="none" stroke="'.$color.'" stroke-width="3" />';
+            announce(3, 7);
+            break;
+    }
+    storeData('left', $possible[0]);
+    return $svg;
+}
+
+function generateTopCarPath($color, $layout){
+    $x = 50; $y = 50;
+    $svg = '';
+    $possible = array();
+    if($layout[3]==ROAD){
+        $possible[] = 3;
+    }
+    if($layout[5]==ROAD){
+        $possible[] = 5;
+    }
+    if($layout[7]==ROAD){
+        $possible[] = 7;
+    }
+    shuffle($possible);
+    if(isset($_GET['top'])) $possible[0]=$_GET['top'];
+    switch($possible[0]){
+        case 7:
+            $svg .= '<polygon id="carpath1" points="' . ($x + 11) . ',' . ($y + 1) . ' ' . ($x + 11) . ',' . ($y + 49) . ' ' . ($x + 14) . ',' . ($y + 49). ' ' . ($x + 14) . ',' . ($y + 1) . '" fill="'.$color.'" />';
+            announce(1,7);
+            break;
+        case 3:
+            $svg .= '<path id="carpath1" d="M'.($x+12.5).','.($y+1).' A12.5,12.5 0 0,1 '.($x+1).','.($y+12.5).'" fill="none" stroke="'.$color.'" stroke-width="3" />';
+            announce(1,3);
+            break;
+        case 5:
+            $svg .= '<path id="carpath1" d="M'.($x+49).','.($y+37.5).' A37,37 0 0,1 '.($x+12.5).','.($y+1).'" fill="none" stroke="'.$color.'" stroke-width="3" />';
+            announce(1,5);
+            break;
+    }
+    storeData('top', $possible[0]);
+    return $svg;
+}
+
+function generateRightCarPath($color, $layout){
+    $x = 50; $y = 50;
+    $svg = '';
+    $possible = array();
+    if($layout[1]==ROAD){
+        $possible[] = 1;
+    }
+    if($layout[3]==ROAD){
+        $possible[] = 3;
+    }
+    if($layout[7]==ROAD){
+        $possible[] = 7;
+    }
+    shuffle($possible);
+    if(isset($_GET['right'])) $possible[0]=$_GET['right'];
+    switch($possible[0]){
+        case 1:
+            $svg .= '<path id="carpath5" d="M'.($x+49).','.($y+12.5).' A12,12 0 0,1 '.($x+37.5).','.($y+1).'" fill="none" stroke="'.$color.'" stroke-width="3" />';
+            announce(5,1);
+            break;
+        case 3:
+            $svg .= '<polygon id="carpath5" points="' . ($x + 1) . ',' . ($y + 14) . ' ' . ($x + 49) . ',' . ($y + 14) . ' ' . ($x + 49) . ',' . ($y + 11). ' ' . ($x + 1) . ',' . ($y + 11) . '" fill="'.$color.'" />';
+            announce(5,3);
+            break;
+        case 7:
+            $svg .= '<path id="carpath5" d="M'.($x+12.5).','.($y+49).' A37.5,37.5 0 0,1 '.($x+49).','.($y+12.5).'" fill="none" stroke="'.$color.'" stroke-width="3" />';
+            announce(5,7);
+            break;
+    }
+    storeData('right', $possible[0]);
+    return $svg;
+}
+
+function generateMainRoad($layout){
+    global $mainRoad;
+    $possible=array();
+
+    if(!rand(0,5)){
+        storeData('mainRoad', '');
+        return $possible;
+    }
+
+    foreach($layout as $index => $piece){
+        if($index==4) continue;
+        if($piece == ROAD){
+            $possible[]=$index;
+        }
+    }
+
+    shuffle($possible);
+    $mainRoad = array_slice($possible, 0, 2);
+    sort($mainRoad);
+
+    if(isset($_GET['mainRoad'])){
+        $mainRoad = preg_split('//', $_GET['mainRoad'], -1, PREG_SPLIT_NO_EMPTY);
+    }
+    storeData('mainRoad', implode('', $mainRoad));
+    return $mainRoad;
+}
+
+function generateArrowSVG($x, $y, $layout) {
+    $possible = array();
+    if($layout[1]==ROAD){
+        $possible[] = 'straight';
+    }
+    if($layout[3]==ROAD){
+        $possible[] = 'left';
+    }
+    if($layout[5]==ROAD){
+        $possible[] = 'right';
+    }
+    shuffle($possible);
+    $direction = array_pop($possible);
+    if(isset($_GET['direction'])){
+        $direction = $_GET['direction'];
+    }
+    storeData('direction', $direction);
+    $svg = '';
+    if ($direction == 'left') {
+        $svg .= '<path id="carpath7" d="M'.($x+1).','.(12.5+$y).' A37,37 0 0,1 '.(37.5+$x).','.(50+$y-1).'" fill="none" stroke="white" stroke-width="3" />';
+        announce(7,3);
+    }
+    elseif ($direction == 'straight') {
+        $svg .= '<polygon id="carpath7" points="' . ($x + 36) . ',' . ($y + 50 -1) . ' ' . ($x + 36) . ',' . ($y + 2) . ' ' . ($x + 39) . ',' . ($y + 2). ' ' . ($x + 39) . ',' . ($y + 50-1) . '" fill="white" />';
+        announce(7,1);
+    }
+    elseif ($direction == 'right') {
+        $svg .= '<path id="carpath7" d="M'.(37.5+$x).','.(50+$y-1).' A12.5,12.5 0 0,1 '.(50+$x-1).','.(37.5+$y).'" fill="none" stroke="white" stroke-width="3" />';
+        announce(7,5);
+    }
+    $svg .= '';
+
+    return $svg;
+}
